f72fff175ba7f030e7274242e613e5d32585c442
FunkyGlitch <corentin_gaspard@ens.univ-artois.fr>
2021-05-28 20:34:16 +0200

    debut du projet

---
+++ gridView.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QWidget
+class gridView(QWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Sokoban")
+        self.window
+        self.window = QWidget()
+        self.show()
+        
+
