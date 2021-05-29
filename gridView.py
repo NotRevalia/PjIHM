453dfe4d3961ecbd501442f6241f7ab468527372
FunkyGlitch <corentin_gaspard@ens.univ-artois.fr>
2021-05-29 14:53:09 +0200

    test afficher image et ajout de ctrMouvement.py

--- gridView.py
+++ gridView.py
@@ -1,12 +1,25 @@
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtGui import QWindow
+from PyQt5.QtWidgets import QWidget ,QMainWindow,QGridLayout,QPushButton
+from PyQt5.QtGui import QPixmap,QImage,QIcon
 
-class gridView(QWindow):
+class gridView(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Sokoban")
-        self.window
-        self.window = QWidget()
-        self.show()
-        
+        self.setFixedSize(500,500)
+        self.__window = QWidget()
+        self.__gridLayout =QGridLayout()
+        self.grifButton = []
+        model = QImage("Fighter_Front.png",'png')
+        self.__model = model
+        self.__controller = None
+    def setModel(self,model):
+        self.__model = model
+
+
+    def setController(self,controller):
+        self.__controller = controller
+
+    def display(self):
+        assert self.__model is not None
+
 
---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication,QLabel
+
+from gridView import *
+
+
+app = QApplication(sys.argv)
+
+windows = gridView()
+#gridView.display()
+windows.show()
+sys.exit(app.exec_())
