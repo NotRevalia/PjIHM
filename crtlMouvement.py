32280d0af0eeb31839c471394c961cf8235f22fa
Max <duboismax62153@gmail.com>
2021-06-11 16:48:49 +0200

    Collisions fini

--- crtlMouvement.py
+++ crtlMouvement.py
@@ -28,47 +28,134 @@ class crtlMouvement(QWidget):
         return self.__view
 
     def checkD(self,a,b):
+        move = False
         plateau = self.__model.getPlateau()
-        if(plateau[a][b+1]==2):
-            plateau[a][b+1]=0
-            plateau[a][b+2]=2
-            print("Caisse")
         if(plateau[a][b+1]==1):
-            self.__son=QSound("quack.wav")
-            self.__son.play()
-            print("Mur")
-
-    def checkH(self,a,b):
-        plateau = self.__model.getPlateau()
-        if (plateau[a-1][b] == 2):
-            plateau[a-1][b]=0
-            plateau[a-2][b]=2
-            print("Caisse")
-        if (plateau[a-1][b] == 1):
             self.__son = QSound("quack.wav")
             self.__son.play()
             print("Mur")
+            return move
+        elif(plateau[a][b+1]==2):
+            if(plateau[a][b+2]==0):
+                plateau[a][b+1]=0
+                plateau[a][b+2]=2
+                print("Caisse")
+                move = True
+
+            elif (plateau[a][b+2]==4):
+                plateau[a][b+1] = 0
+                plateau[a][b+2] = 3
+                move = True
+
+        elif (plateau[a][b+1] == 3):
+            if (plateau[a][b+2] == 0):
+                plateau[a][b+2] = 2
+                plateau[a][b+1] = 4
+                move = True
+
+        elif (plateau[a][b + 1] == 0):
+            move = True
+        elif (plateau[a][b+1] == 4):
+            move = True
+        return move
+
+
+    def checkH(self,a,b):
+        move = False
+        plateau = self.__model.getPlateau()
+        if (plateau[a - 1][b] == 1):
+            self.__son = QSound("quack.wav")
+            self.__son.play()
+            print("Mur")
+            return move
+        elif (plateau[a-1][b] == 2):
+            if(plateau[a-2][b]==0):
+                plateau[a-1][b]=0
+                plateau[a-2][b]=2
+                print("Caisse")
+                move = True
+            elif (plateau[a-2][b] == 4):
+                plateau[a-1][b] = 0
+                plateau[a-2][b] = 3
+                move = True
+
+        elif (plateau[a-1][b] == 3):
+            if (plateau[a-2][b] == 0):
+                plateau[a-2][b] = 2
+                plateau[a-1][b] = 4
+                move = True
+
+        elif (plateau[a-1][b] == 0):
+                move = True
+        elif (plateau[a-1][b] == 4):
+            move = True
+        return move
 
     def checkG(self,a,b):
+        move = False
         plateau = self.__model.getPlateau()
-        if(plateau[a][b-1]==2):
-            plateau[a][b-1]=0
-            plateau[a][b-2]=2
-            print("Caisse")
-        if(plateau[a][b-1]==1):
-            self.__son=QSound("quack.wav")
+        if (plateau[a][b - 1] == 1):
+            self.__son = QSound("quack.wav")
             self.__son.play()
             print("Mur")
+            return move
+        elif (plateau[a][b - 1] == 2):
+            if (plateau[a][b - 2] == 0):
+                plateau[a][b - 1] = 0
+                plateau[a][b - 2] = 2
+                print("Caisse")
+                move = True
+            elif (plateau[a][b - 2] == 4):
+                plateau[a][b - 1] = 0
+                plateau[a][b - 2] = 3
+                move = True
+
+        elif (plateau[a][b-1] == 3):
+            if (plateau[a][b-2] == 0):
+                plateau[a][b-2] = 2
+                plateau[a][b-1] = 4
+                move = True
+
+        elif (plateau[a][b - 1] == 0):
+            move = True
+        elif (plateau[a][b - 1] == 4):
+            move = True
+
+        return move
+
 
     def checkB(self,a,b):
+        move = False
         plateau = self.__model.getPlateau()
-        if(plateau[a+1][b]==2):
-            plateau[a+1][b]=0
-            plateau[a+2][b]=2
-            print("Caisse")
-        if(plateau[a+1][b]==1):
-            self.__son=QSound("quack.wav")
+        if (plateau[a + 1][b] == 1):
+            self.__son = QSound("quack.wav")
             self.__son.play()
             print("Mur")
+            return move
+        elif (plateau[a + 1][b] == 2):
+            if (plateau[a + 2][b] == 0):
+                plateau[a + 1][b] = 0
+                plateau[a + 2][b] = 2
+                print("Caisse")
+                move = True
+
+            elif (plateau[a + 2][b] == 4):
+                plateau[a + 1][b] = 0
+                plateau[a + 2][b] = 3
+                move = True
+
+        elif(plateau[a+1][b]==3):
+            if(plateau[a+2][b]==0):
+                plateau[a+2][b]=2
+                plateau[a+1][b]=4
+                move=True
+
+        elif (plateau[a + 1][b] == 0):
+            move = True
+        elif (plateau[a + 1][b] == 4):
+            move = True
+
+        return move
+
 
 
--- gridView.py
+++ gridView.py
@@ -51,6 +51,11 @@ class gridView(QWidget):
                     im =QtGui.QPixmap("Petite_caisse.png")
                     im = im.scaled(r.size())
                     painter.drawPixmap(r,im)
+                if (case == 3):
+                    r = QtCore.QRect(i * 50, j * 50, 50, 50)
+                    im = QtGui.QPixmap("CaisseE.png")
+                    im = im.scaled(r.size())
+                    painter.drawPixmap(r, im)
                 if (case == 4):
                     r = QtCore.QRect(i * 50, j * 50, 50, 50)
                     im = QtGui.QPixmap("etoile.png")
@@ -63,23 +68,16 @@ class gridView(QWidget):
         if value == 0:
             if self.__controller.checkG(self.__model.getPlayer()[0], self.__model.getPlayer()[1]):
                 self.__model.updatePlayer([self.__model.getPlayer()[0], self.__model.getPlayer()[1]-1])
-                print("Gauche")
-            self.__model.updatePlayer([self.__model.getPlayer()[0], self.__model.getPlayer()[1]-1])
-        if value == 1:
+        elif value == 1:
             if self.__controller.checkH(self.__model.getPlayer()[0], self.__model.getPlayer()[1]):
                 self.__model.updatePlayer([self.__model.getPlayer()[0]-1,self.__model.getPlayer()[1]])
-                print("Haut")
-            self.__model.updatePlayer([self.__model.getPlayer()[0]-1, self.__model.getPlayer()[1]])
-        if value == 2:
+        elif value == 2:
             if self.__controller.checkD(self.__model.getPlayer()[0],self.__model.getPlayer()[1]):
                 self.__model.updatePlayer([self.__model.getPlayer()[0], self.__model.getPlayer()[1]+1])
-                print("Droite")
-            self.__model.updatePlayer([self.__model.getPlayer()[0], self.__model.getPlayer()[1]+1])
-        if value == 3:
+        elif value == 3:
             if self.__controller.checkB(self.__model.getPlayer()[0], self.__model.getPlayer()[1]):
                 self.__model.updatePlayer([self.__model.getPlayer()[0]+1, self.__model.getPlayer()[1]])
-                print("Bas")
-            self.__model.updatePlayer([self.__model.getPlayer()[0] + 1, self.__model.getPlayer()[1]])
+        print(self.__model.getPlayer())
 
     def updateView(self):
         self.update()
